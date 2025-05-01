c04cbfc2c8b551583b4d874d6bf9968a2f9f06db
GiyeonChoi <bdcgy22@daum.net>
2025-05-01 14:47:18 +0900

    init : 초기 커밋

---
+++ src/main/java/com/giyeon/chat_server/ChatServerApplication.java
@@ -0,0 +1,14 @@
+package com.giyeon.chat_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServerApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/giyeon/chat_server/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giyeon.chat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
