93b7df116f089e92f1d85638a8bce02249e897b6
GiyeonChoi <bdcgy22@daum.net>
2025-05-07 17:54:10 +0900

    refactor : AuthController 생성
    - 로그인, 회원가입을 담당하는 controller추가
    - api url 가독성 향상

---
+++ src/main/java/com/giyeon/chat_server/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.giyeon.chat_server.controller;
+
+import com.giyeon.chat_server.dto.ApiResponseDto;
+import com.giyeon.chat_server.dto.AuthTokenDto;
+import com.giyeon.chat_server.dto.LoginRequestDto;
+import com.giyeon.chat_server.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/api/auth/login")
+    public ApiResponseDto<?> login(@RequestBody LoginRequestDto loginDto) {
+
+        AuthTokenDto authTokenDto = loginService.authenticateUser(loginDto);
+
+        return ApiResponseDto.builder()
+                .code(200)
+                .data(authTokenDto)
+                .build();
+    }
+
+    @PostMapping("/api/auth/refresh")
+    public ApiResponseDto<?> refresh(@RequestBody AuthTokenDto refreshToken) {
+
+        AuthTokenDto authTokenDto = loginService.reset(refreshToken.getRefreshToken());
+
+        return ApiResponseDto.builder()
+                .code(200)
+                .data(authTokenDto)
+                .build();
+    }
+
+}
--- src/main/java/com/giyeon/chat_server/controller/UserController.java
+++ src/main/java/com/giyeon/chat_server/controller/UserController.java
@@ -13,29 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final LoginService loginService;
-
-    @PostMapping("/api/login")
-    public ApiResponseDto<?> login(@RequestBody LoginRequestDto loginDto) {
-
-        AuthTokenDto authTokenDto = loginService.authenticateUser(loginDto);
-
-        return ApiResponseDto.builder()
-                .code(200)
-                .data(authTokenDto)
-                .build();
-    }
-
-    @PostMapping("/api/refresh")
-    public ApiResponseDto<?> refresh(@RequestBody AuthTokenDto refreshToken) {
-
-        AuthTokenDto authTokenDto = loginService.reset(refreshToken.getRefreshToken());
-
-        return ApiResponseDto.builder()
-                .code(200)
-                .data(authTokenDto)
-                .build();
-    }
 
 
 }
