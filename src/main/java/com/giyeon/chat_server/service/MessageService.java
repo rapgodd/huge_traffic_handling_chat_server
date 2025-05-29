8734d07164947f1becebc8acc4ec346f7ee004e1
GiyeonChoi <bdcgy22@daum.net>
2025-05-29 15:24:58 +0900

    perf(message): 레디스를 활용한 메세지 전송 성능 개선
    - before: db에서 유저 챗룸을 가져온 후 현재 접속중인 유저인지 아닌지 결정한다.
    - after: 유저 챗룸에 isJoined 컬럼이 변경될때마다 레디스도 동일하게 업데이트.
                이후 현재 접속중인 유저를 가져올 때 레디스에서 바로 꺼내온다.

--- src/main/java/com/giyeon/chat_server/service/MessageService.java
+++ src/main/java/com/giyeon/chat_server/service/MessageService.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.giyeon.chat_server.dto.ChatDto;
 import com.giyeon.chat_server.dto.RoomUsersDto;
-import com.giyeon.chat_server.service.repositoryService.MainRepositoryService;
-import com.giyeon.chat_server.service.repositoryService.MessageRepositoryService;
+import com.giyeon.chat_server.service.msgSender.threadPoolSender.ThreadSendingService;
+import com.giyeon.chat_server.service.redisService.RedisService;
+import com.giyeon.chat_server.service.redisService.repositoryService.MainRepositoryService;
+import com.giyeon.chat_server.service.redisService.repositoryService.MessageRepositoryService;
+import com.giyeon.chat_server.util.IdDistributionUtils;
 import com.giyeon.chat_server.ws.SessionRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,11 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
-import java.net.URI;
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,22 +32,26 @@ public class MessageService{
     private final MainRepositoryService mainRepositoryService;
     private final MessageRepositoryService messageRepositoryService;
     @Autowired
-    private RedisTemplate<String, String> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
     private final ObjectMapper objectMapper;
     private final ThreadPoolExecutor threadPoolExecutor;
     private final ThreadSendingService threadSendingService;
+    @Autowired
+    private RedisService redisService;
 
     public void sendMessage(ChatDto chatDto) {
 
         Long roomId = chatDto.getRoomId();
         String payload = chatDto.getMessage();
         Long senderId = chatDto.getSenderId();
-        ZonedDateTime createdAt = chatDto.getCreatedAt();
+        ZonedDateTime createdAt = ZonedDateTime.now();
 
-        messageRepositoryService.insertMessage(roomId,payload,senderId,createdAt);
+        Long msgId = messageRepositoryService.insertMessage(roomId, payload, senderId, createdAt);
 
         threadPoolExecutor.submit(()->{
             String json;
+            List<Long> currentJoinedUsers = redisService.getCurrentJoinedUsers(roomId);
+            chatDto.setJoinedUser(currentJoinedUsers.size());
             try {
                 json = objectMapper.writeValueAsString(chatDto);
             } catch (JsonProcessingException e) {
@@ -59,44 +63,38 @@ public class MessageService{
 
             List<Long> localSessionUsersList = new ArrayList<>(30);
             List<Long> remoteSessionUsersList = new ArrayList<>(30);
+            List<Long> userIds = new ArrayList<>(30);
 
 
             usersInRoom.getUserChatRoomList().forEach(userChatRoom -> {
-                Long userId = userChatRoom.getUser().getId();
-                if (sessionRegistry.isUserSessionExist(userId)) {
-                      localSessionUsersList.add(userId);
-
-
-                } else {
-
-                    String IP = redisTemplate.opsForValue().get(String.valueOf(userId));
-
-                    if(IP!=null){
-                        remoteSessionUsersList.add(userId);
-                    }
-
-                }
+                userIds.add(userChatRoom.getUser().getId());
             });
-            sendJoinedLocalUsers(localSessionUsersList, roomId, json);
+
+            IdDistributionUtils.distributeRemoteAndLocal(userIds, localSessionUsersList,
+                                remoteSessionUsersList, sessionRegistry, redisTemplate);
+
+            sendJoinedLocalUsers(localSessionUsersList, json);
             sendJoinedRemoteUsers(remoteSessionUsersList, roomId, json);
+
+            // Redis 에 저장된 방의 마지막 메시지 ID 를 업데이트
+            redisService.putUsersLastMsgIdInRoom(currentJoinedUsers, roomId, String.valueOf(msgId));
+            redisService.putLastMsgIdInRoom(roomId, String.valueOf(msgId));
 
         });
 
     }
 
     private void sendJoinedRemoteUsers(List<Long> remoteSessionUsersList, Long roomId, String json) {
-        List<Long> joinedUsersInCurrentRoom = mainRepositoryService.findJoinedUsersInCurrentRoom(remoteSessionUsersList, roomId);
-        for (Long id : joinedUsersInCurrentRoom) {
-            String IP = redisTemplate.opsForValue().get(String.valueOf(id));
+        for (Long id : remoteSessionUsersList) {
+            String IP = (String)redisTemplate.opsForValue().get(String.valueOf(id));
             if(IP!=null){
                 threadSendingService.addMessageToQueue(json,IP.split(":")[0],Integer.parseInt(IP.split(":")[1]), roomId,id);
             }
         }
     }
 
-    private void sendJoinedLocalUsers(List<Long> localSessionUsersList, Long roomId, String json) {
-        List<Long> joinedUsersId = mainRepositoryService.findJoinedUsersInCurrentRoom(localSessionUsersList, roomId);
-        for (Long id : joinedUsersId) {
+    private void sendJoinedLocalUsers(List<Long> localSessionUsersList, String json) {
+        for (Long id : localSessionUsersList) {
             try {
                 sessionRegistry.getUserSession(id).sendMessage(new TextMessage(json));
             } catch (IOException e) {
@@ -104,6 +102,5 @@ public class MessageService{
             }
         }
     }
-
 
 }
