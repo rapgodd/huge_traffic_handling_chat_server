11f9c3005b7b02f6244f1f5fd92d576da1af025d
GiyeonChoi <bdcgy22@daum.net>
2025-06-20 21:34:00 +0900

    refactor(redis): Redis 설정 클래스를 프로덕션/테스트용으로 분리
    - 기존 RedisConfig를 ProdRedisConfig, RedisTestConfig로 분리
    - 프로덕션 환경과 테스트 환경에서 각각 독립적 설정 로드 가능

--- src/main/java/com/giyeon/chat_server/config/ProdRedisConfig.java
+++ src/main/java/com/giyeon/chat_server/config/ProdRedisConfig.java
@@ -1,20 +1,27 @@
 package com.giyeon.chat_server.config;
 
 import io.lettuce.core.ReadFrom;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Profile("!test")
 @Configuration
 @EnableCaching
-public class RedisConfig {
+public class ProdRedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConn(RedisProperties redisProperties){
@@ -30,15 +37,36 @@ public class RedisConfig {
 
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory){
-        RedisTemplate<String, String> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
         StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        GenericJackson2JsonRedisSerializer hashValueSerializer = new GenericJackson2JsonRedisSerializer();
+
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(hashValueSerializer);
+
         template.setKeySerializer(stringSerializer);
         template.setValueSerializer(stringSerializer);
 
         return template;
     }
+
+    @Bean
+    public RedissonClient redissonClient(RedisProperties redisProperties){
+        Config config = new Config();
+        if(redisProperties.getCluster() != null){
+            for(String node : redisProperties.getCluster().getNodes()){
+                config.useClusterServers().addNodeAddress("redis://" + node);
+            }
+        }else{
+            config.useSingleServer()
+                    .setAddress("redis://" + redisProperties.getHost() + ":" + redisProperties.getPort());
+        }
+
+        return Redisson.create(config);
+    }
+
 
 }
---
+++ src/main/java/com/giyeon/chat_server/config/RedisTestConfig.java
@@ -0,0 +1,53 @@
+package com.giyeon.chat_server.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Profile("test")
+@Configuration
+@EnableCaching
+public class RedisTestConfig {
+
+
+    @Bean
+    public LettuceConnectionFactory redisConn(){
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 4000));
+    }
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+
+    @Bean
+    public RedissonClient redissonTestClient(RedisProperties redisProperties){
+        Config config = new Config();
+
+        config.useSingleServer().
+                setAddress("redis://" + "localhost" + ":" + "4000");
+
+        return Redisson.create(config);
+    }
+
+}
+
